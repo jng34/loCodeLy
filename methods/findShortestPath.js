0143183b1caa9ebb7bc359d4350bae6684437dc0
JamesNg <njames15@gmail.com>
2023-04-17 19:05:57 -0400

    installed dotenv and mongoose npm pkgs, added new folders/files

--- methods/findShortestPath.js
+++ methods/findShortestPath.js
@@ -3,20 +3,6 @@ const createZipMap = require("../zipNodes/createZipMap");
 const zipMap = createZipMap();
 const createZipNode = require('../zipNodes/createZipNode')
 
-// const updateUnvisitedZips = (currZip, zipMap) => {
-  
-//   let unvisitedNeighbors = graph[currZip.zipCode]; //not visited
-  
-//   for (let neighbor of unvisitedNeighbors) {
-//     const neighborZip = zipMap.find((zip) => zip.zipCode === String(neighbor));
-//     console.log(neighborZip)
-//     if (neighborZip.isVisited === false) {
-//       neighborZip.distance = currZip.distance + 1;
-//       neighborZip.previousNode = currZip;
-//     }
-//   }
-// }
-
 
 const findShortestPath = (zip1, zip2) => {
   // Find zip1 in zipMap
@@ -29,7 +15,6 @@ const findShortestPath = (zip1, zip2) => {
   const visitedZipsInOrder = [];
   
   while (zipMap.length > 0) {
-    // sortZipsByDistance?
     zipMap.sort((zipA,zipB) => zipA.distance - zipB.distance);
     const currZip = zipMap.shift(); // Takes out front zip from array
     if (currZip.distance === Infinity) continue;
@@ -49,19 +34,6 @@ const findShortestPath = (zip1, zip2) => {
     }
   }
 }
-
-/*
-  TODO
-  -Function works but only gives one absolute path that depends on 
-  our graph inputs.
-  -Need to find all Paths by maybe creating a second map of zips to 
-  modify the distances
-  -Finish the backtracking function to return the path of all zips
-*/
-
-
-// const zips = findShortestPath(10002, 10021);
-// console.log(zips)
 
 
 module.exports = findShortestPath
--- methods/findZipsInBtwn.js
+++ methods/findZipsInBtwn.js
@@ -1,9 +1,5 @@
 const findShortestPath = require('./findShortestPath')
 
-const allZips = findShortestPath(10002, 'Central Park');
-const lastZip = allZips[allZips.length - 1]
-// console.log(lastZip)
-
 const findZipsInBtwn = (endZip) => {
   const zipsInShortestPath = [];
   let currentZip = endZip;
@@ -14,6 +10,9 @@ const findZipsInBtwn = (endZip) => {
   return zipsInShortestPath;
 }
 
-// console.log(findZipsInBtwn(lastZip))
+const allZips = findShortestPath(10002, 'Central Park');
+const lastZip = allZips[allZips.length - 1]
+console.log(findZipsInBtwn(lastZip))
+
 
 module.exports = findZipsInBtwn;
